0d49630a48730ef6ea10b0548429b608368487f7
Tiago Rodrigues <tmcrodrigues@gmail.com>
2016-06-01 17:02:46 +0200

    clean gh-pages branch

--- karma.conf.local.js
+++
@@ -1,9 +0,0 @@
-var karmaBaseConf = require('./karma.base.conf')
-
-module.exports = function (config) {
-  karmaBaseConf.plugins.push(require('karma-chrome-launcher'))
-  karmaBaseConf.browsers = ['Chrome']
-  karmaBaseConf.logLevel = config.LOG_DEBUG
-
-  config.set(karmaBaseConf)
-}
--- test/integration/generate-random-id.js
+++
@@ -1,3 +0,0 @@
-export default function generateRandomId (prefix = 'randomid') {
-  return prefix + Math.ceil(Math.random() * 1e8)
-}
