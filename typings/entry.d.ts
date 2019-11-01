5433474e0d3233c9ded25b6b737a8319bdc76085
Ryan Bruns <ryan.bruns7@gmail.com>
2019-11-01 15:23:02 -0500

    add typings to anywhere an entry is retrieved

--- typings/entry.d.ts
+++ typings/entry.d.ts
@@ -2,10 +2,10 @@ import { DefaultElements } from './defaultElements'
 import { MetaLinkProps, MetaSys, MetaSysProps } from './meta'
 import { Collection } from './collection'
 
-export interface EntryProp {
+export interface EntryProp<TFields = any> {
   fields: {
     [key: string]: any
-  }
+  } & TFields
 }
 
 export interface EntrySys extends MetaSysProps {
@@ -18,11 +18,11 @@ export interface EntrySys extends MetaSysProps {
   publishedCounter?: number,
 }
 
-export interface Entry
-  extends EntryProp,
+export interface Entry<TEntryFields = any>
+  extends EntryProp<TEntryFields>,
     DefaultElements<EntryProp & EntrySys>,
     MetaSys<EntrySys> {
-  archive(): Promise<Entry>,
+  archive<TFieldType = any>(): Promise<Entry<TFieldType>>,
   delete(): Promise<void>,
   getSnapshot(id: string): Promise<any>,
   getSnapshots(): Promise<Collection<any>>,
@@ -30,8 +30,8 @@ export interface Entry
   isDraft(): boolean,
   isPublished(): boolean,
   isUpdated(): boolean,
-  publish(): Promise<Entry>,
-  unarchive(): Promise<Entry>,
-  unpublish(): Promise<Entry>,
-  update(): Promise<Entry>,
+  publish<TFieldType = any>(): Promise<Entry<TFieldType>>,
+  unarchive<TFieldType = any>(): Promise<Entry<TFieldType>>,
+  unpublish<TFieldType = any>(): Promise<Entry<TFieldType>>,
+  update<TFieldType = any>(): Promise<Entry<TFieldType>>,
 }
