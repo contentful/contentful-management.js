04a7ecbf7a73e841e63b2e856c3e475ea43c8bf8
Ryan Bruns <ryan.bruns7@gmail.com>
2019-12-09 10:49:45 -0600

    update typings per review comments

--- typings/entry.d.ts
+++ typings/entry.d.ts
@@ -2,11 +2,11 @@ import { DefaultElements } from './defaultElements'
 import { MetaLinkProps, MetaSys, MetaSysProps } from './meta'
 import { Collection } from './collection'
 
-export interface EntryProp<TFields = {
-  [key: string]: any
-}> {
+export interface EntryProp<TFields> {
   fields: TFields
 }
+
+type KeyValueMap = Record<string, any>;
 
 export interface EntrySys extends MetaSysProps {
   contentType: { sys: MetaLinkProps },
@@ -18,11 +18,11 @@ export interface EntrySys extends MetaSysProps {
   publishedCounter?: number,
 }
 
-export interface Entry<TEntryFields = any>
+export interface Entry<TEntryFields = KeyValueMap>
   extends EntryProp<TEntryFields>,
-    DefaultElements<EntryProp & EntrySys>,
-    MetaSys<EntrySys> {
-  archive<TFieldType = {}>(): Promise<Entry<TFieldType>>,
+  DefaultElements<EntryProp<TEntryFields> & EntrySys>,
+      MetaSys<EntrySys> {
+  archive<TFieldType>(): Promise<Entry<TFieldType>>,
   delete(): Promise<void>,
   getSnapshot(id: string): Promise<any>,
   getSnapshots(): Promise<Collection<any>>,
@@ -30,8 +30,8 @@ export interface Entry<TEntryFields = any>
   isDraft(): boolean,
   isPublished(): boolean,
   isUpdated(): boolean,
-  publish<TFieldType = {}>(): Promise<Entry<TFieldType>>,
-  unarchive<TFieldType = {}>(): Promise<Entry<TFieldType>>,
-  unpublish<TFieldType = {}>(): Promise<Entry<TFieldType>>,
-  update<TFieldType = {}>(): Promise<Entry<TFieldType>>,
+  publish<TFieldType>(): Promise<Entry<TFieldType>>,
+  unarchive<TFieldType>(): Promise<Entry<TFieldType>>,
+  unpublish<TFieldType>(): Promise<Entry<TFieldType>>,
+  update<TFieldType>(): Promise<Entry<TFieldType>>,
 }
