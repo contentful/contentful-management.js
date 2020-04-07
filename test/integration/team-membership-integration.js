0fe0b52d21bd3e5c5de082e7c1aeed8d4c24f806
Victoria Beleuta <vbeleuta@gmail.com>
2020-04-07 10:12:03 +0200

    fix team memberships integration tests

--- test/integration/team-membership-integration.js
+++ test/integration/team-membership-integration.js
@@ -1,7 +1,7 @@
 export default function teamMembershipTests (t, organization) {
   t.test('Gets teamMemberships', (t) => {
     t.plan(3)
-    return organization.getTeamMemberships('4bsQOABPBKerHD35NPGEPh')
+    return organization.getTeamMemberships('7pIEx2fMx53SSR1jd7C46M')
       .then((response) => {
         t.ok(response.sys, 'sys')
         t.ok(response.items, 'items')
@@ -11,11 +11,24 @@ export default function teamMembershipTests (t, organization) {
 
   t.test('Gets teamMembership', (t) => {
     t.plan(3)
-    return organization.getTeamMembership('4bsQOABPBKerHD35NPGEPh', '4NcpxJYamTkuHxWMVqoLaI')
+    return organization.getTeamMembership('7pIEx2fMx53SSR1jd7C46M', '1tzDxN5zD2JbUjwzntAlLW')
       .then((response) => {
         t.ok(response.sys, 'sys')
-        t.ok(response.sys.id, '4NcpxJYamTkuHxWMVqoLaI')
+        t.ok(response.sys.id, '1tzDxN5zD2JbUjwzntAlLW')
         t.ok(response.sys.type, 'TeamMembership')
+      })
+  })
+
+  t.test('Create and delete teamMembership', (t) => {
+    t.plan(3)
+    return organization.createTeamMembership('7pIEx2fMx53SSR1jd7C46M', {
+      admin: true, organizationMembershipId: '5mDCHGHvePI8NOzw0WaY2Z'
+    })
+      .then((teamMembership) => {
+        t.ok(teamMembership.sys, 'sys')
+        t.equal(teamMembership.admin, true)
+        t.ok(teamMembership.sys.type, 'TeamMembership')
+        teamMembership.delete()
       })
   })
 }
