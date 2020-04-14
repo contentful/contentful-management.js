a4ff480eab421e378d7ae9f3ddd1543c60f65303
Victoria Beleuta <vbeleuta@gmail.com>
2020-04-14 13:48:10 +0200

    add team  typings

--- typings/organization.d.ts
+++ typings/organization.d.ts
@@ -3,21 +3,27 @@ import {OrganizationInvitation, OrganizationInvitationProps} from './organizatio
 import { Collection } from './collection'
 import { DefaultElements } from './defaultElements'
 import { MetaSys, MetaSysProps } from './meta'
-import { TeamMembership, TeamMembershipProps } from "./teamMembership";
+import { TeamMembership, TeamMembershipProps } from './teamMembership'
+import { Team, TeamProps } from './team'
 
 export interface OrganizationProp {
   name: string
 }
 
 export interface ContentfulOrganizationAPI {
-  createAppDefinition(data: AppDefinitionProps): Promise<AppDefinition>,
-  getAppDefinition(id: string): Promise<AppDefinition>,
-  getAppDefinitions(): Promise<Collection<AppDefinition>>,
-  createTeamMembership(data: TeamMembershipProps): Promise<TeamMembership>,
-  getTeamMembership(teamId: string, id: string): Promise<TeamMembership>,
-  getTeamMembership(teamId: string): Promise<Collection<TeamMembership>>,
-  getOrganizationInvitation(id: string): Promise<OrganizationInvitation>,
-  createOrganizationInvitation(data: OrganizationInvitationProps): Promise<OrganizationInvitation>
+  createAppDefinition(data: AppDefinitionProps): Promise<AppDefinition>;
+  getAppDefinition(id: string): Promise<AppDefinition>;
+  getAppDefinitions(): Promise<Collection<AppDefinition>>;
+  createTeam(data: TeamMembershipProps): Promise<Team>;
+  getTeam(teamId: string, id: string): Promise<Team>;
+  getTeams(teamId: string): Promise<Collection<Team>>;
+  createTeamMembership(data: TeamMembershipProps): Promise<TeamMembership>;
+  getTeamMembership(teamId: string, id: string): Promise<TeamMembership>;
+  getTeamMemberships(teamId: string): Promise<Collection<TeamMembership>>;
+  getOrganizationInvitation(id: string): Promise<OrganizationInvitation>;
+  createOrganizationInvitation(
+    data: OrganizationInvitationProps
+  ): Promise<OrganizationInvitation>;
 }
 
 export interface Organization extends DefaultElements<OrganizationProp>, OrganizationProp, MetaSys<MetaSysProps>, ContentfulOrganizationAPI {}
