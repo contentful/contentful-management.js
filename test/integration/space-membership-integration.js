6ccf0d1ef43b84eb505125ba8b5c29592e1aebce
Patrycja Radaczyńska <trycjapa@gmail.com>
2020-05-04 12:30:19 +0200

    fix(spaceMemberships): updated integration tests (#312)

--- test/integration/space-membership-integration.js
+++ test/integration/space-membership-integration.js
@@ -1,9 +1,7 @@
 import generateRandomId from './generate-random-id'
 
-// This is a fake user, it actually does not exist.
-const email = 'js-sdk-tests@contentful.com'
-
-export default function spaceMembershipTests (t, space) {
+// To test creating spaceMemberships user has to exist in the organization. Organization membership is produced also within invitation process.
+export default function spaceMembershipTests (t, organization, space) {
   t.test('Gets spaceMemberships', (t) => {
     t.plan(2)
     return space.getSpaceMemberships()
@@ -13,43 +11,66 @@ export default function spaceMembershipTests (t, space) {
       })
   })
 
-  t.skip('Create spaceMembership with id', (t) => {
-    const id = generateRandomId('spaceMembership')
-    return space.getRoles()
-      .then((roles) => {
-        return space.createSpaceMembershipWithId(id, {
-          admin: false,
-          email: email,
-          roles: [
-            { sys: { type: 'Link', linkType: 'Role', id: roles.items[0].sys.id } }
-          ]
-        })
-          .then((spaceMembership) => {
-            t.equals(spaceMembership.sys.id, id, 'id')
-            return spaceMembership.delete()
+  t.test('Create spaceMembership with id', (t) => {
+    return organization.createOrganizationInvitation({
+      email: 'test-spaceMembership-id.user@contentful.com',
+      firstName: 'Test',
+      lastName: 'User',
+      role: 'developer'
+    })
+      .then((response) => organization.getOrganizationInvitation(response.sys.id))
+      .then((invitation) => organization.getOrganizationMembership(invitation.sys.organizationMembership.sys.id))
+      .then((membership) => {
+        const id = generateRandomId('spaceMembership')
+        space.getRoles()
+          .then((roles) => {
+            return space.createSpaceMembershipWithId(id, {
+              admin: false,
+              email: 'test-spaceMembership-id.user@contentful.com',
+              roles: [
+                { sys: { type: 'Link', linkType: 'Role', id: roles.items[0].sys.id } }
+              ]
+            })
+              .then((spaceMembership) => {
+                t.equals(spaceMembership.sys.id, id, 'id')
+                return spaceMembership.delete()
+              })
+              .then(() => {
+                // delete organization membership
+                membership.delete()
+              })
           })
       })
   })
 
-  t.skip('Create spaceMembership', (t) => {
-    return space.getRoles()
-      .then((roles) => {
-        return space.createSpaceMembership({
-          admin: false,
-          email: email,
-          roles: [
-            { sys: { type: 'Link', linkType: 'Role', id: roles.items[0].sys.id } }
-          ]
-        })
-          .then((spaceMembership) => {
-            t.ok(spaceMembership.user, 'user')
-            t.notOk(spaceMembership.admin, 'admin')
-            delete spaceMembership.user
-            spaceMembership.admin = true
-            return spaceMembership.update()
-              .then((updatedSpaceMembership) => {
-                t.ok(spaceMembership.admin, 'admin')
-                return updatedSpaceMembership.delete()
+  t.test('Create spaceMembership', (t) => {
+    return organization.createOrganizationInvitation({
+      email: 'test-spaceMembership.user@contentful.com',
+      firstName: 'Test',
+      lastName: 'User',
+      role: 'developer'
+    })
+      .then((response) => organization.getOrganizationInvitation(response.sys.id))
+      .then((invitation) => organization.getOrganizationMembership(invitation.sys.organizationMembership.sys.id))
+      .then((membership) => {
+        space.getRoles()
+          .then((roles) => {
+            return space.createSpaceMembership({
+              admin: false,
+              email: 'test-spaceMembership.user@contentful.com',
+              roles: [
+                { sys: { type: 'Link', linkType: 'Role', id: roles.items[0].sys.id } }
+              ]
+            })
+              .then((spaceMembership) => {
+                t.ok(spaceMembership.user, 'user')
+                t.notOk(spaceMembership.admin, 'admin')
+                t.equal(spaceMembership.sys.type, 'SpaceMembership', 'type')
+                return spaceMembership.delete()
+              })
+              .then(() => {
+                // delete organization membership
+                membership.delete()
               })
           })
       })
