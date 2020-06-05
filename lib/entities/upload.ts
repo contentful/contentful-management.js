bf07e7a413c1bb681fca3418419c6202ef80af4a
Andreas Hörnicke <andreas.hoernicke@gmail.com>
2020-06-05 11:57:35 +0200

    chore: migrate upload + team* (#349)
    * chore: migrate upload + team*
    
    * fix: code generation and js examples
    
    * fix: prettier

---
+++ lib/entities/upload.ts
@@ -0,0 +1,54 @@
+import cloneDeep from 'lodash/cloneDeep'
+import { freezeSys, toPlainObject } from 'contentful-sdk-core'
+import enhanceWithMethods from '../enhance-with-methods'
+import { createDeleteEntity } from '../instance-actions'
+import { AxiosInstance } from 'axios'
+import { DefaultElements, MetaSysProps } from '../common-types'
+
+export type UploadProps = {
+  /**
+   * System metadata
+   */
+  sys: MetaSysProps
+}
+
+export interface Upload extends UploadProps, DefaultElements<UploadProps> {
+  /**
+   * Deletes this object on the server.
+   * @return Promise for the deletion. It contains no data, but the Promise error case should be handled.
+   * @example
+   * const contentful = require('contentful-management')
+   *
+   * const client = contentful.createClient({
+   *   accessToken: '<content_management_api_key>'
+   * })
+   *
+   * client.getSpace('<space_id>')
+   * .then((space) => space.getUpload('<upload_id>'))
+   * .then((upload) => upload.delete())
+   * .then((upload) => console.log(`upload ${upload.sys.id} updated.`))
+   * .catch(console.error)
+   */
+  delete(): Promise<void>
+}
+
+function createUploadApi(http: AxiosInstance) {
+  return {
+    delete: createDeleteEntity({
+      http: http,
+      entityPath: 'uploads',
+    }),
+  }
+}
+
+/**
+ * @private
+ * @param {Object} http - HTTP client instance
+ * @param {Object} data - Raw upload data
+ * @return {Upload} Wrapped upload data
+ */
+export function wrapUpload(http: AxiosInstance, data: UploadProps) {
+  const upload = toPlainObject(cloneDeep(data))
+  enhanceWithMethods(upload, createUploadApi(http))
+  return freezeSys(upload)
+}
--- typings/upload.d.ts
+++
@@ -1,5 +0,0 @@
-import { MetaSys, MetaSysProps, DefaultElements } from './generated/common-types'
-
-export interface Upload extends MetaSys<MetaSysProps>, DefaultElements<MetaSys<MetaSysProps>> {
-  delete(): Promise<void>
-}
