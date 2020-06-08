93e4519b71394504a0299e7c1214c2b48dead052
burakukula <trycjapa@gmail.com>
2020-06-08 12:35:52 +0200

    Regenerate types

--- typings/generated/entities/organization.d.ts
+++ typings/generated/entities/organization.d.ts
@@ -16,16 +16,16 @@ export declare type OrganizationProp = {
  * http client with an organization id, so the base path for requests now has the
  * organization id already set.
  * @private
- * @param  {Object} http - HTTP client instance
- * @param  {Object} data - API response for a Organization
+ * @param http - HTTP client instance
+ * @param data - API response for an Organization
  * @return {Organization}
  */
 export declare function wrapOrganization(http: AxiosInstance, data: OrganizationProp): any;
 /**
  * This method normalizes each organization in a collection.
  * @private
- * @param {Object} http - HTTP client instance
- * @param {Object} data - Raw organization collection data
+ * @param http - HTTP client instance
+ * @param data - Raw organization collection data
  * @return {OrganizationCollection} Normalized organization collection data
  */
 export declare function wrapOrganizationCollection(http: AxiosInstance, data: CollectionProp<OrganizationProp>): CollectionProp<OrganizationProp> & {
