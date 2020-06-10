a6b30f7525db03cb1746f3412cb2979aca0cbc5f
Alex Suevalov <alex.suevalov@contentful.com>
2020-06-10 12:12:15 +0200

    fix: docs

--- lib/entities/locale.ts
+++ lib/entities/locale.ts
@@ -47,7 +47,7 @@ export interface Locale extends LocaleProps, DefaultElements<LocaleProps> {
   /**
    * Deletes this object on the server.
    * @return Promise for the deletion. It contains no data, but the Promise error case should be handled.
-   * ```javascript
+   * @example ```javascript
    * const contentful = require('contentful-management')
    *
    * const client = contentful.createClient({
@@ -65,7 +65,7 @@ export interface Locale extends LocaleProps, DefaultElements<LocaleProps> {
   /**
    * Sends an update to the server with any changes made to the object's properties
    * @return Object returned from the server with updated changes.
-   * ```javascript
+   * @example ```javascript
    * const contentful = require('contentful-management')
    *
    * const client = contentful.createClient({
