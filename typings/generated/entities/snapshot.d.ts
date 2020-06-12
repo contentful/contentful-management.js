a6886da54ed3c7f7da9bc1ce30a52613e5a56053
Alexander Suevalov <alex.suevalov@contentful.com>
2020-06-12 10:07:56 +0200

    chore: convert entry, content-type and snapshot to typescript (#361)

---
+++ typings/generated/entities/snapshot.d.ts
@@ -0,0 +1,29 @@
+import { AxiosInstance } from 'axios';
+import { MetaSysProps, DefaultElements, CollectionProp } from '../common-types';
+export declare type SnapshotProps<T> = {
+    sys: MetaSysProps & {
+        snapshotType: string;
+        snapshotEntityType: string;
+    };
+    snapshot: T;
+};
+export interface Snapshot<T> extends SnapshotProps<T>, DefaultElements<SnapshotProps<T>> {
+}
+/**
+ * @private
+ * @param http - HTTP client instance
+ * @param data - Raw snapshot data
+ * @return Wrapped snapshot data
+ */
+export declare function wrapSnapshot<T>(_http: AxiosInstance, data: SnapshotProps<T>): SnapshotProps<T> & {
+    toPlainObject(): SnapshotProps<T>;
+};
+/**
+ * @private
+ * @param http - HTTP client instance
+ * @param data - Raw snapshot collection data
+ * @return Wrapped snapshot collection data
+ */
+export declare function wrapSnapshotCollection<T>(http: AxiosInstance, data: CollectionProp<SnapshotProps<T>>): CollectionProp<SnapshotProps<T>> & {
+    toPlainObject(): CollectionProp<SnapshotProps<T>>;
+};
--- typings/snapshot.d.ts
+++
@@ -1,8 +0,0 @@
-import { MetaSys, MetaSysProps } from './generated/common-types'
-import { ContentTypeProps } from './contentType'
-import { EntryProp } from './entry'
-
-export interface Snapshot<T extends ContentTypeProps | EntryProp> {
-  sys: MetaSys<MetaSysProps & { snapshotType: string; snapshotEntityType: string }>
-  snapshot: T & MetaSys<MetaSysProps>
-}
