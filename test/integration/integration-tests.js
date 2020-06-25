c83bba6cd79544a86e6f0fb3206e6b17f34c2a13
Alex Suevalov <alex.suevalov@contentful.com>
2020-06-25 09:21:29 +0200

    test: fix integration tests

--- test/integration/integration-tests.js
+++ test/integration/integration-tests.js
@@ -251,40 +251,22 @@ test('Create space for tests of space membership', (t) => {
 })
 
 test('Create space for tests which create, change and delete data', (t) => {
-  return (
-    client
-      .createSpace(
-        {
-          name: 'CMA JS SDK tests',
-        },
-        organization
-      )
-      // When running these tests locally, create a specific space, uncomment and
-      // use the line below to avoid running into the 10 space per hour creation limit.
-      // Also comment the test.onFinish line below to avoid removing the space.
-      // The below line also uses double quotes on purpose so it breaks the linter
-      // in case someone forgets to comment this line again.
-      // client.getSpace('a3f19zbn5ldg')
-      .then((space) => {
-        return space
-          .createLocale({
-            name: 'German (Germany)',
-            code: 'de-DE',
-          })
-          .then(() => {
-            return space
-          })
-      })
-      .then((space) => {
-        test.onFinish(() => space.delete())
-        return Promise.all([
-          webhookTests(t, space),
-          roleTests(t, space),
-          apiKeyTests(t, space),
-          environmentTests(t, space, waitForEnvironmentToBeReady),
-        ])
-      })
-  )
+  return client
+    .createSpace(
+      {
+        name: 'CMA JS SDK tests',
+      },
+      organization
+    )
+    .then((space) => {
+      test.onFinish(() => space.delete())
+      return Promise.all([
+        webhookTests(t, space),
+        roleTests(t, space),
+        apiKeyTests(t, space),
+        environmentTests(t, space, waitForEnvironmentToBeReady),
+      ])
+    })
 })
 
 function waitForEnvironmentToBeReady(space, environment) {
