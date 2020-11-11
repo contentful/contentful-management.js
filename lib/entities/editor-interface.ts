49b4c3af1ebaf6cfda2035c73a8d78e7f15f833d
John Whiles <john.whiles@contentful.com>
2020-11-11 10:54:09 +0100

    feat: add editors and sidebar to EditorInterface type

--- lib/entities/editor-interface.ts
+++ lib/entities/editor-interface.ts
@@ -18,6 +18,16 @@ export interface Control {
   settings?: Record<string, any>
 }
 
+export interface Editor {
+  widgetId: string
+  widgetNamespace: string
+  /**
+   * Widget will be enabled if disabled property is missing
+   */
+  disabled?: boolean
+  settings?: Record<string, any>
+}
+
 export type EditorInterfaceProps = {
   sys: MetaSysProps & {
     space: { sys: MetaLinkProps }
@@ -28,6 +38,14 @@ export type EditorInterfaceProps = {
    * Array of fields and it's associated widgetId
    */
   controls: Control[]
+  /**
+   * Array of editors. Defaults will be used if property is missing.
+   */
+  editors?: Editor[]
+  /**
+   * Array of sidebar widgerts. Defaults will be used if property is missing.
+   */
+  sidebar?: Editor[]
 }
 
 export interface EditorInterface
@@ -69,6 +87,9 @@ export interface EditorInterface
    * .then((contentType) => contentType.getEditorInterface())
    * .then((editorInterface) => {
    *  editorInterface.controls[0] = { "fieldId": "title", "widgetId": "singleLine"}
+   *  editorInterface.editors = [
+   *    { "widgetId": "custom-widget", "widgetNamespace": "app" }
+   *  ]
    *  return editorInterface.update()
    * })
    * .catch(console.error)
