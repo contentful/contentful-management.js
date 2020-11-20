8eebb09a492f46764a39c0ded795baf2e4673513
Alexander Suevalov <alex.suevalov@contentful.com>
2020-11-20 17:09:09 +0100

    feat: a single entry for all external type definitions (#559)

---
+++ lib/export-types.ts
@@ -0,0 +1,82 @@
+export * from './common-types'
+
+export type { ApiKey, ApiKeyProps, CreateApiKeyProps } from './entities/api-key'
+export type {
+  AppDefinition,
+  AppDefinitionProps,
+  AppLocation,
+  CreateAppDefinitionProps,
+} from './entities/app-definition'
+export type {
+  AppInstallation,
+  AppInstallationProps,
+  CreateAppInstallationProps,
+} from './entities/app-installation'
+export type { Asset, AssetProps, CreateAssetProps, AssetFileProp } from './entities/asset'
+export type { ContentType, ContentTypeProps, CreateContentTypeProps } from './entities/content-type'
+export type { ContentFields, ContentTypeFieldValidation } from './entities/content-type-fields'
+export type {
+  EditorInterface,
+  Editor,
+  EditorInterfaceProps,
+  Control,
+} from './entities/editor-interface'
+export type { Entry, EntryProps, CreateEntryProps } from './entities/entry'
+export type { Environment, EnvironmentProps, CreateEnvironmentProps } from './entities/environment'
+export type {
+  EnvironmentAlias,
+  EnvironmentAliasProps,
+  CreateEnvironmentAliasProps,
+} from './entities/environment-alias'
+export type { Locale, LocaleProps, CreateLocaleProps } from './entities/locale'
+export type { Organization, OrganizationProp } from './entities/organization'
+export type {
+  OrganizationInvitation,
+  OrganizationInvitationProps,
+  CreateOrganizationInvitationProps,
+} from './entities/organization-invitation'
+export type {
+  OrganizationMembership,
+  OrganizationMembershipProps,
+} from './entities/organization-membership'
+export type {
+  PersonalAccessToken,
+  PersonalAccessTokenProp,
+  CreatePersonalAccessTokenProps,
+} from './entities/personal-access-token'
+export type { PreviewApiKey, PreviewApiKeyProps } from './entities/preview-api-key'
+export type { Role, RoleProps, CreateRoleProps } from './entities/role'
+export type { ScheduledAction, ScheduledActionProps } from './entities/scheduled-action'
+export type { Snapshot, SnapshotProps } from './entities/snapshot'
+export type { Space, SpaceProps } from './entities/space'
+export type { SpaceMember, SpaceMemberProps } from './entities/space-member'
+export type {
+  SpaceMembership,
+  SpaceMembershipProps,
+  CreateSpaceMembershipProps,
+} from './entities/space-membership'
+export type { Team, TeamProps, CreateTeamProps } from './entities/team'
+export type {
+  TeamMembership,
+  TeamMembershipProps,
+  CreateTeamMembershipProps,
+} from './entities/team-membership'
+export type { Tag, TagProps, CreateTagProps } from './entities/tag'
+export type {
+  TeamSpaceMembership,
+  TeamSpaceMembershipProps,
+  CreateTeamSpaceMembershipProps,
+} from './entities/team-space-membership'
+export type { UIExtension, UIExtensionProps, CreateUIExtensionProps } from './entities/ui-extension'
+export type { Upload, UploadProps } from './entities/upload'
+export type { Usage, UsageProps } from './entities/usage'
+export type { User, UserProps } from './entities/user'
+export type {
+  WebHooks,
+  WebhookProps,
+  WebhookCallRequest,
+  WebhookTransformation,
+  WebhookFilter,
+  CreateWebhooksProps,
+  UpdateWebhookProps,
+} from './entities/webhook'
