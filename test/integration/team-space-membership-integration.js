9a3cdf0f2805d7b328e9648438b5290370faf170
Marco Link <marco.link@contentful.com>
2020-11-25 09:31:33 +0100

    Add note for skipped test

--- test/integration/team-space-membership-integration.js
+++ test/integration/team-space-membership-integration.js
@@ -2,7 +2,8 @@ import { after, before, describe, test } from 'mocha'
 import { client, createTestSpace } from '../helpers'
 import { expect } from 'chai'
 
-describe('TeamSpaceMembership api', function () {
+// Skipping the while suite for now to not create unused spaces.
+describe.skip('TeamSpaceMembership api', function () {
   let space
 
   before(async () => {
@@ -15,6 +16,11 @@ describe('TeamSpaceMembership api', function () {
     }
   })
 
+  /*
+  TODO: To make this test work, we first need to put the space in the required state.
+  - create a team, receive id, and use this id for this test.
+  - pull role id's dynamically from roles.
+   */
   test.skip('Create, update and delete teamSpaceMembership', async () => {
     return space
       .createTeamSpaceMembership('5vllqmpyrhlgaz0xb2S90C', {
