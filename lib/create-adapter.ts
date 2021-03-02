995c90b2de6b3e28389a84d2672512351d386726
Andi Pätzold <andreas.paetzold@contentful.com>
2021-03-02 17:47:19 +0100

    feat: rename cmaAdapter to apiAdapter

--- lib/create-adapter.ts
+++ lib/create-adapter.ts
@@ -7,15 +7,15 @@ import { Adapter } from './common-types'
 import { RestAdapter, RestAdapterParams } from './adapters/REST/rest-adapter'
 
 export type AdapterParams = {
-  cmaAdapter: Adapter
+  apiAdapter: Adapter
 }
 
 /**
  * @private
  */
 export function createAdapter(params: RestAdapterParams & AdapterParams): Adapter {
-  if (params.cmaAdapter) {
-    return params.cmaAdapter
+  if (params.apiAdapter) {
+    return params.apiAdapter
   } else {
     return new RestAdapter(params)
   }
