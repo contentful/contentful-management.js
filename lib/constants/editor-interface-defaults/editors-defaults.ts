7e7aa76656c3eadf7559c1f2e8a7ddf490ae8ccc
Lorenz Weiß <34910067+loweisz@users.noreply.github.com>
2021-05-20 14:20:49 +0200

    feat: add editor interface defaults (#857)
    Adds the collection of the default lists of widgets for the EditorInterface, having one single source of truth and be used in multiple places.
    
    Sidebar and Editors are exported default constants. For controls there is an exported function that will return the default control of a field.

---
+++ lib/constants/editor-interface-defaults/editors-defaults.ts
@@ -0,0 +1,39 @@
+import { DEFAULT_EDITOR_ID, WidgetNamespace } from './types'
+
+export const EntryEditorWidgetTypes = {
+  DEFAULT_EDITOR: {
+    name: 'Editor',
+    id: DEFAULT_EDITOR_ID,
+    icon: 'Entry',
+  },
+  REFERENCE_TREE: {
+    name: 'References',
+    id: 'reference-tree',
+    icon: 'References',
+  },
+  TAGS_EDITOR: {
+    name: 'Tags',
+    id: 'tags-editor',
+    icon: 'Tags',
+  },
+}
+
+const DefaultEntryEditor = {
+  widgetId: EntryEditorWidgetTypes.DEFAULT_EDITOR.id,
+  widgetNamespace: WidgetNamespace.EDITOR_BUILTIN,
+  name: EntryEditorWidgetTypes.DEFAULT_EDITOR.name,
+}
+
+const ReferencesEntryEditor = {
+  widgetId: EntryEditorWidgetTypes.REFERENCE_TREE.id,
+  widgetNamespace: WidgetNamespace.EDITOR_BUILTIN,
+  name: EntryEditorWidgetTypes.REFERENCE_TREE.name,
+}
+
+const TagsEditor = {
+  widgetId: EntryEditorWidgetTypes.TAGS_EDITOR.id,
+  widgetNamespace: WidgetNamespace.EDITOR_BUILTIN,
+  name: EntryEditorWidgetTypes.TAGS_EDITOR.name,
+}
+
+export const EntryConfiguration = [DefaultEntryEditor, ReferencesEntryEditor, TagsEditor]
---
+++ lib/constants/editor-interface-defaults/index.ts
@@ -0,0 +1,10 @@
+import { SidebarAssetConfiguration, SidebarEntryConfiguration } from './sidebar-defaults'
+import { EntryConfiguration } from './editors-defaults'
+import getDefaultControlOfField from './controls-defaults'
+
+export default {
+  SidebarEntryConfiguration,
+  SidebarAssetConfiguration,
+  EntryConfiguration,
+  getDefaultControlOfField,
+}
