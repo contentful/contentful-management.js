eb56ec9eaf6e48b8c96f603971fb41fabf1f7842
Lorenz Weiß <34910067+loweisz@users.noreply.github.com>
2021-05-20 14:20:49 +0200

    feat: add editor interface defaults (#857)
    Adds the collection of the default lists of widgets for the EditorInterface, having one single source of truth and be used in multiple places.
    
    Sidebar and Editors are exported default constants. For controls there is an exported function that will return the default control of a field.

---
+++ lib/constants/editor-interface-defaults/controls-defaults.ts
@@ -0,0 +1,188 @@
+import { ContentFields, ContentTypeFieldValidation } from '../../entities/content-type-fields'
+import { in_ } from './types'
+
+const DROPDOWN_TYPES = ['Text', 'Symbol', 'Integer', 'Number', 'Boolean']
+
+const INTERNAL_TO_API = {
+  Symbol: { type: 'Symbol' },
+  Text: { type: 'Text' },
+  RichText: { type: 'RichText' },
+  Integer: { type: 'Integer' },
+  Number: { type: 'Number' },
+  Boolean: { type: 'Boolean' },
+  Date: { type: 'Date' },
+  Location: { type: 'Location' },
+  Object: { type: 'Object' },
+  File: { type: 'File' },
+
+  Entry: { type: 'Link', linkType: 'Entry' },
+  Asset: { type: 'Link', linkType: 'Asset' },
+
+  Symbols: { type: 'Array', items: { type: 'Symbol' } },
+  Entries: { type: 'Array', items: { type: 'Link', linkType: 'Entry' } },
+  Assets: { type: 'Array', items: { type: 'Link', linkType: 'Asset' } },
+} as const
+
+export const FIELD_TYPES = Object.keys(INTERNAL_TO_API) as Array<keyof typeof INTERNAL_TO_API>
+
+/**
+ * Returns an internal string identifier for an API field object.
+ *
+ * We use this string as a simplified reference to field types.
+ * Possible values are:
+ *
+ * - Symbol
+ * - Symbols
+ * - Text
+ * - RichText
+ * - Integer
+ * - Number
+ * - Boolean
+ * - Date
+ * - Location
+ * - Object
+ * - Entry
+ * - Entries
+ * - Asset
+ * - Assets
+ * - File
+ */
+export function toInternalFieldType(api: Partial<ContentFields>) {
+  return FIELD_TYPES.find((key) => {
+    const internalApi = INTERNAL_TO_API[key]
+    const stripped = {
+      type: api.type,
+      linkType: api.linkType,
+      items: api.items,
+    }
+    if (stripped.items) {
+      stripped.items = { type: stripped.items.type, linkType: stripped.items.linkType }
+    }
+
+    if (internalApi.type === 'Link') {
+      return internalApi.linkType === stripped.linkType
+    }
+
+    if (internalApi.type === 'Array' && internalApi.items && stripped.items) {
+      if (internalApi.items.type === 'Link') {
+        return internalApi.items.linkType === stripped.items.linkType
+      }
+      return internalApi.items.type === stripped.items.type
+    }
+
+    return internalApi.type === stripped.type
+  })
+}
+
+export const DEFAULTS_WIDGET = {
+  Text: { widgetId: 'markdown' },
+  Symbol: { widgetId: 'singleLine' },
+  Integer: { widgetId: 'numberEditor' },
+  Number: { widgetId: 'numberEditor' },
+  Boolean: { widgetId: 'boolean' },
+  Date: { widgetId: 'datePicker' },
+  Location: { widgetId: 'locationEditor' },
+  Object: { widgetId: 'objectEditor' },
+  RichText: { widgetId: 'richTextEditor' },
+  Entry: { widgetId: 'entryLinkEditor' },
+  Asset: { widgetId: 'assetLinkEditor' },
+  Symbols: { widgetId: 'tagEditor' },
+  Entries: { widgetId: 'entryLinksEditor' },
+  Assets: { widgetId: 'assetLinksEditor' },
+  File: { widgetId: 'fileEditor' },
+}
+
+export const DEFAULTS_SETTINGS = {
+  Boolean: {
+    falseLabel: 'No',
+    helpText: null,
+    trueLabel: 'Yes',
+  },
+  Date: {
+    helpText: null,
+    ampm: '24',
+    format: 'timeZ',
+  },
+  Entry: {
+    helpText: null,
+    showCreateEntityAction: true,
+    showLinkEntityAction: true,
+  },
+  Asset: {
+    helpText: null,
+    showCreateEntityAction: true,
+    showLinkEntityAction: true,
+  },
+
+  Entries: {
+    helpText: null,
+    bulkEditing: false,
+    showCreateEntityAction: true,
+    showLinkEntityAction: true,
+  },
+  Assets: {
+    helpText: null,
+    showCreateEntityAction: true,
+    showLinkEntityAction: true,
+  },
+} as const
+
+interface DefaultWidget {
+  widgetId: string
+  settings?: {
+    helpText: null | string
+  }
+  fieldId: string
+  widgetNamespace: 'builtin'
+}
+
+function getDefaultWidget(field: keyof typeof DEFAULTS_WIDGET, fieldId: string) {
+  const defaultWidget: DefaultWidget = {
+    ...(DEFAULTS_WIDGET[field] as Pick<DefaultWidget, 'widgetId'>),
+    settings: {
+      helpText: null,
+    },
+    widgetNamespace: 'builtin',
+    fieldId,
+  }
+  if (in_(field, DEFAULTS_SETTINGS)) {
+    defaultWidget.settings = {
+      ...defaultWidget.settings,
+      ...DEFAULTS_SETTINGS[field],
+    }
+  }
+  return defaultWidget
+}
+
+// Given our internal identifier returns a minimal API field object.
+export function toApiFieldType(internal: keyof typeof INTERNAL_TO_API) {
+  return INTERNAL_TO_API[internal]
+}
+
+/*
+ * Gets the default widget ID for a field:
+ * - If a field allows predefined values then `dropdown` widget is used
+ *   in the presence of the `in` validation.
+ * - If a Text field is a title then the `singleLine` widget is used.
+ * - Otherwise a simple type-to-editor mapping is used.
+ */
+export default function getDefaultControlOfField(field: ContentFields) {
+  const fieldType = toInternalFieldType(field)
+
+  if (!fieldType) {
+    throw new Error('Invalid field type')
+  }
+
+  const hasInValidation = (field.validations || []).find(
+    (v: ContentTypeFieldValidation) => 'in' in v
+  )
+
+  if (hasInValidation && DROPDOWN_TYPES.includes(fieldType)) {
+    return {
+      widgetId: 'dropdown',
+      fieldId: field.id,
+      widgetNameSpace: 'builtin',
+    }
+  }
+  return getDefaultWidget(fieldType, field.id)
+}
---
+++ test/unit/constants/controls-defaults-test.js
@@ -0,0 +1,93 @@
+import { expect } from 'chai'
+import { it, describe, test } from 'mocha'
+import getDefaultControlOfField, {
+  DEFAULTS_WIDGET,
+  FIELD_TYPES,
+  toApiFieldType,
+} from '../../../lib/constants/editor-interface-defaults/controls-defaults'
+
+describe('controlsDefaults', () => {
+  test('with an unsupported field type', () => {
+    const field = { type: 'unsupportedtype' }
+    expect(function () {
+      getDefaultControlOfField(field)
+    }).to.throw(Error)
+  })
+
+  describe('if validations exist but are different', () => {
+    const validations = [{ size: { max: 500, min: 0 } }]
+
+    it('for a type with a dropdown widget', () => {
+      const field = { type: 'Symbol', validations }
+      expect(getDefaultControlOfField(field).widgetId).equals('singleLine')
+    })
+
+    it('for a type with no dropdown widget', () => {
+      const field = { type: 'Date', validations }
+      expect(getDefaultControlOfField(field).widgetId).equals('datePicker')
+    })
+  })
+
+  describe('if validations exist', () => {
+    const validations = [{ in: ['123'] }]
+
+    it('for a type with a dropdown widget', () => {
+      const field = { type: 'Symbol', validations }
+      expect(getDefaultControlOfField(field).widgetId).equals('dropdown')
+    })
+
+    it('for a type with no dropdown widget', () => {
+      const field = { type: 'Date', validations }
+      expect(getDefaultControlOfField(field).widgetId).equals('datePicker')
+    })
+  })
+
+  it('if field is Text', () => {
+    const field = { type: 'Text', id: 'textfield' }
+    expect(getDefaultControlOfField(field).widgetId).equals('markdown')
+  })
+
+  it('if field is RichText', () => {
+    const field = { type: 'RichText' }
+    expect(getDefaultControlOfField(field).widgetId).equals('richTextEditor')
+  })
+
+  it('if field is Entry', () => {
+    const field = { type: 'Link', linkType: 'Entry' }
+    expect(getDefaultControlOfField(field).widgetId).equals('entryLinkEditor')
+  })
+
+  it('if field is Asset', () => {
+    const field = { type: 'Link', linkType: 'Asset' }
+    expect(getDefaultControlOfField(field).widgetId).equals('assetLinkEditor')
+  })
+
+  it('if field is a list of Assets', () => {
+    const field = { type: 'Array', items: { type: 'Link', linkType: 'Asset' } }
+    expect(getDefaultControlOfField(field).widgetId).equals('assetLinksEditor')
+  })
+
+  it('if field is a list of Entries', () => {
+    const field = { type: 'Array', items: { type: 'Link', linkType: 'Entry' } }
+    expect(getDefaultControlOfField(field).widgetId).equals('entryLinksEditor')
+  })
+
+  it('if field is a File', () => {
+    const field = { type: 'File' }
+    expect(getDefaultControlOfField(field).widgetId).equals('fileEditor')
+  })
+
+  it('has the correct namespace', () => {
+    const field = { type: 'Symbol' }
+    expect(getDefaultControlOfField(field).widgetNamespace).equals('builtin')
+  })
+
+  it('returns default widget ID for each known field type', () => {
+    FIELD_TYPES.forEach((type) => {
+      const defaultWidget = getDefaultControlOfField(toApiFieldType(type))
+      expect(defaultWidget.widgetId).equals(DEFAULTS_WIDGET[type].widgetId)
+    })
+  })
+
+  it('declares default widget IDs for all known field types', () => {})
+})
