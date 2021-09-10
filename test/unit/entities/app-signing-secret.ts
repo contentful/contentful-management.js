e0965aa5edfeaa591cd0d020f008cbabc814d3c3
John Whiles <john.whiles@contentful.com>
2021-09-10 13:28:41 +0100

    fix: signing secret unit tests

--- test/unit/entities/app-signing-secret.ts
+++ test/unit/entities/app-signing-secret.ts
@@ -1,20 +1,37 @@
-import { cloneMock } from '../mocks/entities'
 import setupHttpMock from '../mocks/http'
-import { wrapAppSigningSecret } from '../../../lib/entities/app-signing-secret'
-import { entityWrappedTest } from '../test-creators/instance-entity-methods'
+import { wrapAppSignedRequest } from '../../../lib/entities/app-signed-request'
+import {
+  entityWrappedTest,
+  entityDeleteTest,
+  failingActionTest,
+} from '../test-creators/instance-entity-methods'
+import { appSigningSecretMock } from '../mocks/entities'
 import { describe, test } from 'mocha'
 
 function setup(promise) {
   return {
     httpMock: setupHttpMock(promise),
-    entityMock: cloneMock('assetKey'),
+    entityMock: appSigningSecretMock,
   }
 }
 
 describe('App SigningSecret', () => {
   test('SigningSecret is wrapped', async () => {
     return entityWrappedTest(setup, {
-      wrapperMethod: wrapAppSigningSecret,
+      wrapperMethod: wrapAppSignedRequest,
+    })
+  })
+
+  test('SigningSecret delete', async () => {
+    return entityDeleteTest(setup, {
+      wrapperMethod: wrapAppSignedRequest,
+    })
+  })
+
+  test('SigningSecret delete fails', async () => {
+    return failingActionTest(setup, {
+      wrapperMethod: wrapAppSignedRequest,
+      actionMethod: 'delete',
     })
   })
 })
