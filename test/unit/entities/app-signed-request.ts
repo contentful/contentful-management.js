b66c7ec5c8b7daa611ae8000dce881a07097c838
John Whiles <john.whiles@contentful.com>
2021-09-10 13:24:06 +0100

    feat: use correct mock in app sign request unit

--- test/unit/entities/app-signed-request.ts
+++ test/unit/entities/app-signed-request.ts
@@ -1,13 +1,13 @@
-import { cloneMock } from '../mocks/entities'
 import setupHttpMock from '../mocks/http'
 import { wrapAppSignedRequest } from '../../../lib/entities/app-signed-request'
 import { entityWrappedTest } from '../test-creators/instance-entity-methods'
+import { appSignedRequestMock } from '../mocks/entities'
 import { describe, test } from 'mocha'
 
 function setup(promise) {
   return {
     httpMock: setupHttpMock(promise),
-    entityMock: cloneMock('assetKey'),
+    entityMock: appSignedRequestMock,
   }
 }
 
