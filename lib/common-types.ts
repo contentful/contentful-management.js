05febd0c461e3604ae46b43ef95bc7e906eca7ec
Antigoni Makri <makanti@gmail.com>
2021-09-22 09:21:28 +0100

    fix: add missing prop to interface

--- lib/common-types.ts
+++ lib/common-types.ts
@@ -158,6 +158,7 @@ export interface EntityMetaSysProps extends MetaSysProps {
   publishedAt?: string
   firstPublishedAt?: string
   publishedCounter?: number
+  locale?: string
 }
 
 export interface MetaLinkProps {
