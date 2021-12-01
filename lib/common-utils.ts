22f1e5ecfa98146426a6e9c43ab7adf4eccab78a
Johannes Würbach <johannes.wuerbach@googlemail.com>
2021-12-01 10:37:12 +0100

    fix: contentful-sdk-core v6.10.4 (#1098)
    * fix: contentful-sdk-core v6.10.4
    
    * chore: run prettier

--- lib/common-utils.ts
+++ lib/common-utils.ts
@@ -7,12 +7,12 @@ import { Collection, CollectionProp, MakeRequest } from './common-types'
 /**
  * @private
  */
-export const wrapCollection = <R, T, Rest extends any[]>(
-  fn: (makeRequest: MakeRequest, entity: T, ...rest: Rest) => R
-) => (makeRequest: MakeRequest, data: CollectionProp<T>, ...rest: Rest): Collection<R, T> => {
-  const collectionData = toPlainObject(copy(data))
-  // @ts-expect-error
-  collectionData.items = collectionData.items.map((entity) => fn(makeRequest, entity, ...rest))
-  // @ts-expect-error
-  return collectionData
-}
+export const wrapCollection =
+  <R, T, Rest extends any[]>(fn: (makeRequest: MakeRequest, entity: T, ...rest: Rest) => R) =>
+  (makeRequest: MakeRequest, data: CollectionProp<T>, ...rest: Rest): Collection<R, T> => {
+    const collectionData = toPlainObject(copy(data))
+    // @ts-expect-error
+    collectionData.items = collectionData.items.map((entity) => fn(makeRequest, entity, ...rest))
+    // @ts-expect-error
+    return collectionData
+  }
