0d2884e67c6a3ea742a56c59daf0216466ca17eb
Thomas Kellermeier <thomas.kellermeier@contentful.com>
2021-12-10 11:42:24 +0100

    style: use better type for optional locale props

--- lib/entities/locale.ts
+++ lib/entities/locale.ts
@@ -134,8 +134,7 @@ function createLocaleApi(makeRequest: MakeRequest) {
  * @return Wrapped locale data
  */
 export function wrapLocale(makeRequest: MakeRequest, data: LocaleProps): Locale {
-  type LocalePropsWithOptionalCode = Omit<LocaleProps, 'internal_code'> & { internal_code?: string }
-  delete (data as LocalePropsWithOptionalCode).internal_code
+  delete (data as SetOptional<LocaleProps, 'internal_code'>).internal_code
   const locale = toPlainObject(copy(data))
   const localeWithMethods = enhanceWithMethods(locale, createLocaleApi(makeRequest))
   return freezeSys(localeWithMethods)
