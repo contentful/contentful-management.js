0f5653bb4bad94b42dbc462576c70e6c2a87335b
ronaldronson <ronaldronson@users.noreply.github.com>
2022-01-05 09:59:15 +0100

    feat: add support for wf definition query [] (#1137)
    * feat: add support for wf definition query
    * feat: rename getAll -> getMany

--- lib/plain/plain-client.ts
+++ lib/plain/plain-client.ts
@@ -386,6 +386,7 @@ const addAlphaFeatures = (
   if (alphaFeatures?.includes(ALPHA_FEATURE_WORKFLOWS)) {
     alphaInterface.workflowDefinition = {
       get: wrap(wrapParams, 'WorkflowDefinition', 'get'),
+      getMany: wrap(wrapParams, 'WorkflowDefinition', 'getMany'),
       create: wrap(wrapParams, 'WorkflowDefinition', 'create'),
       update: wrap(wrapParams, 'WorkflowDefinition', 'update'),
       delete: wrap(wrapParams, 'WorkflowDefinition', 'delete'),
