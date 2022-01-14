0798677365ca4c664dcd40d6ec329b475e6be8a9
Daniel Vasylenko <connect.danv@gmail.com>
2022-01-14 14:36:58 +0100

    test: add tests for getOrganization and organizations.get

--- test/integration/organization-integration.ts
+++ test/integration/organization-integration.ts
@@ -2,16 +2,24 @@
 import { expect } from 'chai'
 import { describe, test } from 'mocha'
 import { TestDefaults } from '../defaults'
-import { initPlainClient, initClient } from '../helpers'
+import { initPlainClient, initClient, getTestOrganization } from '../helpers'
 
 describe('Organization API', async function () {
   describe('Contentful client', () => {
+    const client = initClient()
+
     test('getOrganizations', async () => {
-      const client = initClient()
       const collection = await client.getOrganizations()
       const collectionWithLimit = await client.getOrganizations({ limit: 100 })
       expect(collection.limit).to.equal(25)
       expect(collectionWithLimit.limit).to.equal(100)
+    })
+
+    test('getOrganization', async () => {
+      const testOrg = await getTestOrganization()
+      const fetchedOrg = await client.getOrganization(testOrg.sys.id)
+
+      expect(testOrg.toPlainObject()).to.deep.equal(fetchedOrg.toPlainObject())
     })
   })
 
@@ -21,13 +29,20 @@ describe('Organization API', async function () {
       spaceId: TestDefaults.spaceId,
     }
 
+    const plainClient = initPlainClient(defaultParams)
+
     test('getAll', async () => {
-      const plainClient = initPlainClient(defaultParams)
       const orgsCollection = await plainClient.organization.getAll()
       const orgsLimitCollection = await plainClient.organization.getAll({ query: { limit: 100 } })
 
       expect(orgsCollection.limit).to.equal(25)
       expect(orgsLimitCollection.limit).to.equal(100)
+    })
+
+    test('get', async () => {
+      const testOrg = await getTestOrganization()
+      const fetchedOrg = await plainClient.organization.get({ organizationId: testOrg.sys.id })
+      expect(testOrg.toPlainObject()).to.deep.equal(fetchedOrg)
     })
   })
 })
