e9057f4e2d6e63250274096c46e891d1f60ff69d
Daniel Vasylenko <connect.danv@gmail.com>
2022-01-14 15:04:30 +0100

    test: cover error case for organization.get and getOrganization

--- test/integration/organization-integration.ts
+++ test/integration/organization-integration.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { expect } from 'chai'
 import { describe, test } from 'mocha'
 import { TestDefaults } from '../defaults'
@@ -8,18 +7,30 @@ describe('Organization API', async function () {
   describe('Contentful client', () => {
     const client = initClient()
 
-    test('getOrganizations', async () => {
-      const collection = await client.getOrganizations()
-      const collectionWithLimit = await client.getOrganizations({ limit: 100 })
-      expect(collection.limit).to.equal(25)
-      expect(collectionWithLimit.limit).to.equal(100)
+    describe('getOrganizations', () => {
+      test('should return all organizations', async () => {
+        const collection = await client.getOrganizations()
+        const collectionWithLimit = await client.getOrganizations({ limit: 100 })
+        expect(collection.limit).to.equal(25)
+        expect(collectionWithLimit.limit).to.equal(100)
+      })
     })
 
-    test('getOrganization', async () => {
-      const testOrg = await getTestOrganization()
-      const fetchedOrg = await client.getOrganization(testOrg.sys.id)
+    describe('getOrganization', () => {
+      test('should return the organization by id', async () => {
+        const testOrg = await getTestOrganization()
+        const fetchedOrg = await client.getOrganization(testOrg.sys.id)
 
-      expect(testOrg.toPlainObject()).to.deep.equal(fetchedOrg.toPlainObject())
+        expect(testOrg.toPlainObject()).to.deep.equal(fetchedOrg.toPlainObject())
+      })
+
+      test('should return undefined if the organization wasn\t found', async () => {
+        try {
+          await client.getOrganization('nonExistingId')
+        } catch (e) {
+          expect(e.message).to.include('No organization was found with the ID nonExistingId')
+        }
+      })
     })
   })
 
@@ -31,18 +42,30 @@ describe('Organization API', async function () {
 
     const plainClient = initPlainClient(defaultParams)
 
-    test('getAll', async () => {
-      const orgsCollection = await plainClient.organization.getAll()
-      const orgsLimitCollection = await plainClient.organization.getAll({ query: { limit: 100 } })
+    describe('getAll', () => {
+      test('should return all organizations', async () => {
+        const orgsCollection = await plainClient.organization.getAll()
+        const orgsLimitCollection = await plainClient.organization.getAll({ query: { limit: 100 } })
 
-      expect(orgsCollection.limit).to.equal(25)
-      expect(orgsLimitCollection.limit).to.equal(100)
+        expect(orgsCollection.limit).to.equal(25)
+        expect(orgsLimitCollection.limit).to.equal(100)
+      })
     })
 
-    test('get', async () => {
-      const testOrg = await getTestOrganization()
-      const fetchedOrg = await plainClient.organization.get({ organizationId: testOrg.sys.id })
-      expect(testOrg.toPlainObject()).to.deep.equal(fetchedOrg)
+    describe('get', () => {
+      test('should get the organization by id', async () => {
+        const testOrg = await getTestOrganization()
+        const fetchedOrg = await plainClient.organization.get({ organizationId: testOrg.sys.id })
+        expect(testOrg.toPlainObject()).to.deep.equal(fetchedOrg)
+      })
+
+      test("should throw if organization wasn't found", async () => {
+        try {
+          await plainClient.organization.get({ organizationId: 'nonExistingId' })
+        } catch (e) {
+          expect(e.message).to.include('No organization was found with the ID nonExistingId')
+        }
+      })
     })
   })
 })
