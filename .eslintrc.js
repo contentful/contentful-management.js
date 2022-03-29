89026309927096eee7bd168c6225b2f597052ba1
Fabian Heymann <fabianheymann1010@gmail.com>
2022-03-29 08:25:15 +0200

    chore: Update prettier (#1256)
    * build(deps-dev): bump eslint-config-prettier from 6.15.0 to 8.5.0
    
    Bumps [eslint-config-prettier](https://github.com/prettier/eslint-config-prettier) from 6.15.0 to 8.5.0.
    - [Release notes](https://github.com/prettier/eslint-config-prettier/releases)
    - [Changelog](https://github.com/prettier/eslint-config-prettier/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/prettier/eslint-config-prettier/compare/v6.15.0...v8.5.0)
    
    ---
    updated-dependencies:
    - dependency-name: eslint-config-prettier
      dependency-type: direct:development
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * chore: Eslint config compatibility with latest prettier config
    
    * chore: Update prettier to v2.6
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,7 +24,6 @@ module.exports = {
         'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
         'prettier',
-        'prettier/@typescript-eslint',
       ],
       plugins: ['promise', 'no-only-tests'],
       globals: {
