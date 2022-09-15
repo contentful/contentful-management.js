a03def3cd9b348fe5b76afe8aec66d6a6a3ca990
Cyberxon <marouenbs93@gmail.com>
2022-09-15 11:36:27 +0200

    feat: adding new xspace fields definitions

--- lib/entities/field-type.ts
+++ lib/entities/field-type.ts
@@ -10,6 +10,8 @@ export type FieldType =
   | { type: 'Location' }
   | { type: 'Link'; linkType: 'Asset' }
   | { type: 'Link'; linkType: 'Entry' }
+  | { type: 'ResourceLink'; linkType: 'Contentful:entry' }
   | { type: 'Array'; items: { type: 'Symbol' } }
   | { type: 'Array'; items: { type: 'Link'; linkType: 'Entry' } }
+  | { type: 'Array'; items: { type: 'Resourcelink'; linkType: 'Contentful:entry' } }
   | { type: 'Array'; items: { type: 'Link'; linkType: 'Asset' } }
