b01c4afc06d7b32a16bc4275da824a1a5afd40ff
Marco Link <marco.link@contentful.com>
2022-09-20 16:35:31 +0200

    fix(organizationMemberships): query many paginated [ZEND-2681]

--- test/integration/organization-membership-integration.js
+++ test/integration/organization-membership-integration.js
@@ -26,4 +26,21 @@ describe('OrganizationMembership Api', function () {
       expect(response.sys.type).equals('OrganizationMembership')
     })
   })
+
+  test('Gets organizationMemberships paged', async () => {
+    return organization
+      .getOrganizationMemberships({
+        query: {
+          limit: 1,
+          skip: 1,
+        },
+      })
+      .then((response) => {
+        console.dir(response, { depth: Infinity })
+        expect(response.sys, 'sys').ok
+        expect(response.limit).equals(1)
+        expect(response.skip).equals(1)
+        expect(response.items.length).equals(1)
+      })
+  })
 })
