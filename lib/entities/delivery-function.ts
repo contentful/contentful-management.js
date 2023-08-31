3f0a450f55b75caeacca26f87739f9b7c756a3e1
Piotr Ciazynski <piotr.ciazynski@contentful.com>
2023-08-31 16:41:50 +0200

    feat: add delivery function entity and types

---
+++ lib/entities/delivery-function.ts
@@ -0,0 +1,18 @@
+import { Link } from '../common-types'
+
+export type DeliveryFunctionProps = {
+  sys: {
+    id: string
+    type: 'DeliveryFunction'
+    createdBy: Link<'User'> // Only users can CRUD
+    createdAt: string
+    updatedBy: Link<'User'> // Only users can CRUD
+    updatedAt: string
+    organization: Link<'Organization'>
+    appDefinition: Link<'AppDefinition'>
+  }
+  name: string
+  description: string
+  path: string
+  allowNetworks: string[]
+}
