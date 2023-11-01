6952f5e3bb15c5c13d0d75959c0ba0f2f9cdcf62
Tyler Collins <tyler.collins@contentful.com>
2023-11-01 14:11:40 -0600

    test: add unit tests for webhook signing secrets

---
+++ test/unit/plain/webhook-test.ts
@@ -0,0 +1,43 @@
+import { expect } from 'chai'
+import { describe, test } from 'mocha'
+import sinon from 'sinon'
+import { createClient } from '../../../lib/contentful-management'
+import setupRestAdapter from '../adapters/REST/helpers/setupRestAdapter'
+
+describe('Webhook', () => {
+  const spaceId = 'space-id'
+
+  test('getSigningSecret', async () => {
+    const { httpMock, adapterMock } = setupRestAdapter(
+      Promise.resolve({ data: { redactedValue: 'abcd' } })
+    )
+    const plainClient = createClient({ apiAdapter: adapterMock }, { type: 'plain' })
+    const response = await plainClient.webhook.getSigningSecret({ spaceId })
+
+    expect(response).to.be.an('object')
+    expect(response.redactedValue).to.equal('abcd')
+
+    sinon.assert.calledWith(httpMock.get, `/spaces/space-id/webhook_settings/signing_secret`)
+  })
+
+  test('upsertSigningSecret', async () => {
+    const { httpMock, adapterMock } = setupRestAdapter(
+      Promise.resolve({ data: { redactedValue: 'abcd' } })
+    )
+    const plainClient = createClient({ apiAdapter: adapterMock }, { type: 'plain' })
+    const response = await plainClient.webhook.upsertSigningSecret({ spaceId })
+
+    expect(response).to.be.an('object')
+    expect(response.redactedValue).to.equal('abcd')
+
+    sinon.assert.calledWith(httpMock.put, `/spaces/space-id/webhook_settings/signing_secret`)
+  })
+
+  test('deleteSigningSecret', async () => {
+    const { httpMock, adapterMock } = setupRestAdapter(Promise.resolve({ data: '' }))
+    const plainClient = createClient({ apiAdapter: adapterMock }, { type: 'plain' })
+    await plainClient.webhook.deleteSigningSecret({ spaceId })
+
+    sinon.assert.calledWith(httpMock.delete, `/spaces/space-id/webhook_settings/signing_secret`)
+  })
+})
