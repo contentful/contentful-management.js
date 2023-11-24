5b647756fee2225860f5e53bf724390783853494
Piotr Ciazynski <piotr.ciazynski@contentful.com>
2023-11-24 17:03:56 +0100

    feat(trex): propagate headers [MONET-1496]

--- lib/adapters/REST/endpoints/environment-template.ts
+++ lib/adapters/REST/endpoints/environment-template.ts
@@ -3,35 +3,44 @@ import { SetOptional } from 'type-fest'
 import { EnvironmentTemplateProps } from '../../../entities/environment-template'
 import { RestEndpoint } from '../types'
 import * as raw from './raw'
+import { RawAxiosRequestHeaders } from 'axios'
 
 const apiPath = (organizationId: string, ...pathSegments: (number | string)[]) =>
   `/organizations/${organizationId}/environment_templates/` + pathSegments.join('/')
 
 export const get: RestEndpoint<'EnvironmentTemplate', 'get'> = (
   http,
-  { organizationId, environmentTemplateId, version, query = {} }
+  { organizationId, environmentTemplateId, version, query = {} },
+  headers?: RawAxiosRequestHeaders
 ) =>
   version
     ? raw.get(http, apiPath(organizationId, environmentTemplateId, 'versions', version), {
         params: query,
+        headers,
       })
-    : raw.get(http, apiPath(organizationId, environmentTemplateId), { params: query })
+    : raw.get(http, apiPath(organizationId, environmentTemplateId), {
+        params: query,
+        headers,
+      })
 
 export const getMany: RestEndpoint<'EnvironmentTemplate', 'getMany'> = (
   http,
-  { organizationId, query = {} }
-) => raw.get(http, apiPath(organizationId), { params: query })
+  { organizationId, query = {} },
+  headers?: RawAxiosRequestHeaders
+) => raw.get(http, apiPath(organizationId), { params: query, headers })
 
 export const create: RestEndpoint<'EnvironmentTemplate', 'create'> = (
   http,
   { organizationId },
-  payload
-) => raw.post(http, apiPath(organizationId), payload)
+  payload,
+  headers?: RawAxiosRequestHeaders
+) => raw.post(http, apiPath(organizationId), payload, { headers })
 
 export const update: RestEndpoint<'EnvironmentTemplate', 'update'> = (
   http,
   { organizationId, environmentTemplateId },
-  payload
+  payload,
+  headers?: RawAxiosRequestHeaders
 ) => {
   const data: SetOptional<EnvironmentTemplateProps, 'sys'> = copy(payload)
   delete data.sys
@@ -39,6 +48,7 @@ export const update: RestEndpoint<'EnvironmentTemplate', 'update'> = (
   return raw.put(http, apiPath(organizationId, environmentTemplateId), data, {
     headers: {
       'X-Contentful-Version': payload.sys.version ?? 0,
+      ...headers,
     },
   })
 }
@@ -46,48 +56,64 @@ export const update: RestEndpoint<'EnvironmentTemplate', 'update'> = (
 export const versionUpdate: RestEndpoint<'EnvironmentTemplate', 'versionUpdate'> = (
   http,
   { organizationId, version, environmentTemplateId },
-  payload
-) => raw.patch(http, apiPath(organizationId, environmentTemplateId, 'versions', version), payload)
+  payload,
+  headers?: RawAxiosRequestHeaders
+) =>
+  raw.patch(http, apiPath(organizationId, environmentTemplateId, 'versions', version), payload, {
+    headers,
+  })
 
 export const del: RestEndpoint<'EnvironmentTemplate', 'delete'> = (
   http,
-  { organizationId, environmentTemplateId }
-) => raw.del(http, apiPath(organizationId, environmentTemplateId))
+  { organizationId, environmentTemplateId },
+  headers?: RawAxiosRequestHeaders
+) => raw.del(http, apiPath(organizationId, environmentTemplateId), { headers })
 
 export const versions: RestEndpoint<'EnvironmentTemplate', 'versions'> = (
   http,
-  { organizationId, environmentTemplateId, query = {} }
-) => raw.get(http, apiPath(organizationId, environmentTemplateId, 'versions'), { params: query })
+  { organizationId, environmentTemplateId, query = {} },
+  headers?: RawAxiosRequestHeaders
+) =>
+  raw.get(http, apiPath(organizationId, environmentTemplateId, 'versions'), {
+    params: query,
+    headers,
+  })
 
 export const validate: RestEndpoint<'EnvironmentTemplate', 'validate'> = (
   http,
   { spaceId, environmentId, environmentTemplateId, version },
-  payload
+  payload,
+  headers?: RawAxiosRequestHeaders
 ) =>
   raw.put(
     http,
     version
       ? `/spaces/${spaceId}/environments/${environmentId}/template_installations/${environmentTemplateId}/versions/${version}/validated`
       : `/spaces/${spaceId}/environments/${environmentId}/template_installations/${environmentTemplateId}/validated`,
-    payload
+    payload,
+    { headers }
   )
 
 export const install: RestEndpoint<'EnvironmentTemplate', 'install'> = (
   http,
   { spaceId, environmentId, environmentTemplateId },
-  payload
+  payload,
+  headers?: RawAxiosRequestHeaders
 ) =>
   raw.post(
     http,
     `/spaces/${spaceId}/environments/${environmentId}/template_installations/${environmentTemplateId}/versions`,
-    payload
+    payload,
+    { headers }
   )
 
 export const disconnect: RestEndpoint<'EnvironmentTemplate', 'disconnect'> = (
   http,
-  { spaceId, environmentId, environmentTemplateId }
+  { spaceId, environmentId, environmentTemplateId },
+  headers?: RawAxiosRequestHeaders
 ) =>
   raw.del(
     http,
-    `/spaces/${spaceId}/environments/${environmentId}/template_installations/${environmentTemplateId}`
+    `/spaces/${spaceId}/environments/${environmentId}/template_installations/${environmentTemplateId}`,
+    { headers }
   )
