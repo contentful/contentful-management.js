ec2a6f6538ab43f7ad6547ee4b7bf91eb283ebbb
david-shibley-contentful <149433784+david-shibley-contentful@users.noreply.github.com>
2024-02-20 11:49:38 -0700

    feat: PlainClient docs - upload [EXT-4964] (#2178)
    * feat: PlainClient docs - upload [EXT-4964]
    
    * removing old code

--- lib/plain/common-types.ts
+++ lib/plain/common-types.ts
@@ -1,6 +1,5 @@
 import { RawAxiosRequestHeaders, RawAxiosRequestConfig } from 'axios'
 import { OpPatch } from 'json-patch'
-import { Stream } from 'stream'
 import {
   CollectionProp,
   GetAppDefinitionParams,
@@ -30,7 +29,6 @@ import {
   GetEnvironmentTemplateParams,
   BasicCursorPaginationOptions,
   EnvironmentTemplateParams,
-  GetSpaceEnvironmentUploadParams,
 } from '../common-types'
 import { ApiKeyProps, CreateApiKeyProps } from '../entities/api-key'
 import {
@@ -164,6 +162,7 @@ import { AppSigningSecretPlainClientAPI } from './entities/app-signing-secret'
 import { ExtensionPlainClientAPI } from './entities/extension'
 import { AppEventSubscriptionPlainClientAPI } from './entities/app-event-subscription'
 import { AppKeyPlainClientAPI } from './entities/app-key'
+import { UploadPlainClientAPI } from './entities/upload'
 
 export type PlainClientAPI = {
   raw: {
@@ -530,14 +529,7 @@ export type PlainClientAPI = {
       data: CreateAssetKeyProps
     ): Promise<AssetKeyProps>
   }
-  upload: {
-    get(params: OptionalDefaults<GetSpaceEnvironmentUploadParams>): Promise<any>
-    create(
-      params: OptionalDefaults<GetSpaceEnvironmentParams>,
-      data: { file: string | ArrayBuffer | Stream }
-    ): Promise<any>
-    delete(params: OptionalDefaults<GetSpaceEnvironmentUploadParams>): Promise<any>
-  }
+  upload: UploadPlainClientAPI
   locale: {
     get(
       params: OptionalDefaults<GetSpaceEnvironmentParams & { localeId: string }>
