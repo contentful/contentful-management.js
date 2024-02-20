71a561c367819258efcfb1cfecb0c9e41e8f2711
david-shibley-contentful <149433784+david-shibley-contentful@users.noreply.github.com>
2024-02-20 13:16:10 -0700

    feat: PlainClient docs - user [EXT-4960] (#2179)

--- lib/plain/common-types.ts
+++ lib/plain/common-types.ts
@@ -87,7 +87,6 @@ import {
   TeamSpaceMembershipProps,
 } from '../entities/team-space-membership'
 import { UsageProps } from '../entities/usage'
-import { UserProps } from '../entities/user'
 import { DefaultParams, OptionalDefaults } from './wrappers/wrap'
 import { AssetKeyProps, CreateAssetKeyProps } from '../entities/asset-key'
 import { FunctionProps } from '../entities/function'
@@ -162,6 +161,7 @@ import { AppSigningSecretPlainClientAPI } from './entities/app-signing-secret'
 import { ExtensionPlainClientAPI } from './entities/extension'
 import { AppEventSubscriptionPlainClientAPI } from './entities/app-event-subscription'
 import { AppKeyPlainClientAPI } from './entities/app-key'
+import { UserPlainClientAPI } from './entities/user'
 import { UploadPlainClientAPI } from './entities/upload'
 
 export type PlainClientAPI = {
@@ -391,19 +391,7 @@ export type PlainClientAPI = {
       fieldId: string
     ): Promise<ContentTypeProps>
   }
-  user: {
-    getManyForSpace(
-      params: OptionalDefaults<GetSpaceParams & QueryParams>
-    ): Promise<CollectionProp<UserProps>>
-    getForSpace(params: OptionalDefaults<GetSpaceParams & { userId: string }>): Promise<UserProps>
-    getCurrent<T = UserProps>(params?: QueryParams): Promise<T>
-    getForOrganization(
-      params: OptionalDefaults<GetOrganizationParams & { userId: string }>
-    ): Promise<UserProps>
-    getManyForOrganization(
-      params: OptionalDefaults<GetOrganizationParams & QueryParams>
-    ): Promise<CollectionProp<UserProps>>
-  }
+  user: UserPlainClientAPI
   entry: {
     getPublished<T extends KeyValueMap = KeyValueMap>(
       params: OptionalDefaults<GetSpaceEnvironmentParams & QueryParams>,
