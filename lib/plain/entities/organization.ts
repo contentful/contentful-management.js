20e918dd841d34d573bb9e0486aefa9828f6ab7a
Tyler Collins <tyler.collins@contentful.com>
2024-02-20 11:53:37 -0700

    docs: add TSDocs for the organization entity on the plain client interface

---
+++ lib/plain/entities/organization.ts
@@ -0,0 +1,36 @@
+import { PaginationQueryParams, CollectionProp, GetOrganizationParams } from '../../common-types'
+import { OrganizationProp } from '../../entities/organization'
+import { OptionalDefaults } from '../wrappers/wrap'
+
+export type OrganizationPlainClientAPI = {
+  /**
+   * Fetch all organizations the user has access to
+   * @param params Optional pagination query parameters
+   * @returns A collections of organizations
+   * @throws if the request fails, or no organizations are found
+   * @example
+   * ```javascript
+   * const organizations = await client.organization.getAll({
+   *   query: {
+   *     limit: 10,
+   *   }
+   * })
+   * ```
+   */
+  getAll(
+    params?: OptionalDefaults<PaginationQueryParams>
+  ): Promise<CollectionProp<OrganizationProp>>
+  /**
+   * Fetch a single organization by its ID
+   * @param params the organization ID
+   * @returns the requested organization
+   * @throws if the request fails, or the organization is not found
+   * @example
+   * ```javascript
+   * const organization = await client.organization.get({
+   *    organizationId: '<organization_id>'
+   * })
+   * ```
+   */
+  get(params: OptionalDefaults<GetOrganizationParams>): Promise<OrganizationProp>
+}
