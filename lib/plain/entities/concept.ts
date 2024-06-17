e24b79c46778b529c34eddf5ec395c261263db5f
Marco Link <marco.link@contentful.com>
2024-06-17 15:10:23 +0200

    feat: JSDoc for concept entity [GROOT-1273]

--- lib/plain/entities/concept.ts
+++ lib/plain/entities/concept.ts
@@ -1,6 +1,8 @@
 import {
   CursorPaginatedCollectionProp,
+  GetConceptDescendantsParams,
   GetConceptParams,
+  GetManyConceptParams,
   GetOrganizationParams,
 } from '../../common-types'
 import { ConceptProps, CreateConceptProps } from '../../entities/concept'
@@ -8,27 +10,111 @@ import { OptionalDefaults } from '../wrappers/wrap'
 import { Patch } from 'json-patch'
 
 export type ConceptPlainClientAPI = {
+  /**
+   * Create Concept
+   * @param payload the Concept
+   * @returns the created Concept
+   * @throws if the request fails
+   * @example
+   * ```javascript
+   * const concept = await client.concept.create({
+   *   organizationId: '<organization_id>',
+   * }, conceptProps);
+   * ```
+   */
   create(
     params: OptionalDefaults<GetOrganizationParams>,
     payload: CreateConceptProps
   ): Promise<ConceptProps>
 
-  update(
-    params: OptionalDefaults<GetOrganizationParams> & GetConceptParams,
-    payload: Patch
-  ): Promise<ConceptProps>
+  /**
+   * Update Concept
+   * @param Concept patch
+   * @returns the updated Concept
+   * @throws if the request fails
+   * @example
+   * ```javascript
+   * const updatedConcept = await client.concept.update({
+   *   organizationId: '<organization_id>',
+   * }, conceptPatch);
+   * ```
+   */
+  update(params: OptionalDefaults<GetConceptParams>, payload: Patch): Promise<ConceptProps>
 
-  get(params: OptionalDefaults<GetOrganizationParams> & GetConceptParams): Promise<ConceptProps>
+  /**
+   * Get Concept
+   * @param Concept ID
+   * @returns the Concept
+   * @throws if the request fails
+   * @example
+   * ```javascript
+   * const concept = await client.concept.get({
+   *   organizationId: '<organization_id>',
+   *   conceptId: '<concept_id>',
+   * });
+   * ```
+   */
+  get(params: OptionalDefaults<GetConceptParams>): Promise<ConceptProps>
 
-  delete(params: OptionalDefaults<GetOrganizationParams> & GetConceptParams): Promise<unknown>
+  /**
+   * Delete Concept
+   * @param Concept ID
+   * @returns nothing
+   * @throws if the request fails
+   * @example
+   * ```javascript
+   * await client.concept.update({
+   *   organizationId: '<organization_id>',
+   *   conceptId: '<concept_id>',
+   * });
+   * ```
+   */
+  delete(params: OptionalDefaults<GetConceptParams>): Promise<unknown>
 
+  /**
+   * Get many Concepts
+   * @param query by string or ConceptScheme ID
+   * @returns list of many Concepts
+   * @throws if the request fails
+   * @example
+   * ```javascript
+   * const concepts = await client.concept.getMany({
+   *   organizationId: '<organization_id>',
+   * });
+   * ```
+   */
   getMany(
-    params: OptionalDefaults<GetOrganizationParams>
+    params: OptionalDefaults<GetManyConceptParams>
   ): Promise<CursorPaginatedCollectionProp<ConceptProps>>
 
+  /**
+   * Get number of total Concept
+   * @param Concept ID
+   * @returns number of total Concept
+   * @throws if the request fails
+   * @example
+   * ```javascript
+   * const {total} = await client.concept.getTotal({
+   *   organizationId: '<organization_id>',
+   * });
+   * ```
+   */
   getTotal(params: OptionalDefaults<GetOrganizationParams>): Promise<{ total: number }>
 
+  /**
+   * Get descendant Concepts
+   * @param Concept ID
+   * @returns list of Concepts
+   * @throws if the request fails
+   * @example
+   * ```javascript
+   * const concepts = await client.concept.getDescendants({
+   *   organizationId: '<organization_id>',
+   *   conceptId: '<concept_id>',
+   * });
+   * ```
+   */
   getDescendants(
-    params: OptionalDefaults<GetOrganizationParams>
+    params: OptionalDefaults<GetConceptDescendantsParams>
   ): Promise<CursorPaginatedCollectionProp<ConceptProps>>
 }
