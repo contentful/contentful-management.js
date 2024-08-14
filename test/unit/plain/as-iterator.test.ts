a162af011ecdebe2dea96e29c4dffbd3bda5ea4d
Benedikt Rötsch <benedikt@hashbite.net>
2024-08-14 12:50:39 +0200

    test: refactor plain iterator tests

--- test/unit/plain/as-iterator.test.ts
+++ test/unit/plain/as-iterator.test.ts
@@ -1,11 +1,10 @@
-import { test } from 'mocha'
-import sinon from 'sinon'
+import { vi, test, expect } from 'vitest'
 import defaultsDeep from 'lodash/defaultsDeep'
 import { asIterator } from '../../../lib/plain/as-iterator'
-import { expect } from 'chai'
+import { CollectionProp } from '../../../lib/common-types'
 
 const exhaustIterator = async (iterator) => {
-  const data = []
+  const data: unknown[] = []
 
   for await (const x of iterator) {
     data.push(x)
@@ -16,12 +15,15 @@ const exhaustIterator = async (iterator) => {
 
 const defaultLimit = 2
 
-const createIterableEndpoint = (items) => {
-  return (params) => {
+const createIterableEndpoint = (items): ((params: unknown) => Promise<CollectionProp<unknown>>) => {
+  return async (params) => {
     const {
       query: { skip, limit },
     } = defaultsDeep(params, { query: { limit: defaultLimit, skip: 0 } })
     return {
+      sys: {
+        type: 'Array',
+      },
       items: items.slice(skip, skip + limit),
       total: items.length,
       skip,
@@ -32,20 +34,21 @@ const createIterableEndpoint = (items) => {
 
 test('returns all items when exhausted', async () => {
   const items = [1, 2, 3, 4, 5]
-  const iterableFn = sinon.spy(createIterableEndpoint(items))
+  const iterableFn = createIterableEndpoint(items)
   const actualItems = await exhaustIterator(asIterator(iterableFn, {}))
   expect(actualItems).to.eql(items)
 })
 
 test('adheres limit param for pagination', async function () {
   const items = [1, 2, 3]
-  const iterableFn = sinon.spy(createIterableEndpoint(items))
+  const iterableFn = createIterableEndpoint(items)
+  const spy = vi.fn(iterableFn)
 
-  const actualItems = await exhaustIterator(asIterator(iterableFn, { query: { limit: 1 } }))
+  const actualItems = await exhaustIterator(asIterator(spy, { query: { limit: 1 } }))
   expect(actualItems).to.eql([1, 2, 3])
-  expect(iterableFn.callCount).to.eql(3)
+  expect(spy.mock.calls).toHaveLength(3)
 
-  expect(iterableFn.calledWith({ query: { limit: 1, skip: 0 } })).to.ok
-  expect(iterableFn.calledWith({ query: { limit: 1, skip: 1 } })).to.ok
-  expect(iterableFn.calledWith({ query: { limit: 1, skip: 2 } })).to.ok
+  expect(spy).toHaveBeenNthCalledWith(1, { query: { limit: 1, skip: 0 } })
+  expect(spy).toHaveBeenNthCalledWith(2, { query: { limit: 1, skip: 1 } })
+  expect(spy).toHaveBeenNthCalledWith(3, { query: { limit: 1, skip: 2 } })
 })
