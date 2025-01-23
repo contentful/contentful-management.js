85718c0e1de96d6819807083088578840e0eb41a
Benedikt Rötsch <benedikt@hashbite.net>
2025-01-23 13:53:22 +0100

    style: format code to make prettier lint happy

--- test/integration/app-bundle-integration.test.ts
+++ test/integration/app-bundle-integration.test.ts
@@ -1,13 +1,7 @@
 import { expect, describe, test, beforeAll, afterAll } from 'vitest'
 import { readFileSync } from 'fs'
 import { getTestOrganization, getDefaultSpace, timeoutToCalmRateLimiting } from '../helpers'
-import type {
-  Organization,
-  AppDefinition,
-  AppUpload,
-  Space,
-  Environment,
-} from '../../lib'
+import type { Organization, AppDefinition, AppUpload, Space, Environment } from '../../lib'
 
 describe('AppBundle api', { sequential: true }, () => {
   let organization: Organization
