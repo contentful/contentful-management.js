60b4409a249cbe8aab8da8fbc828ea360f2f4efe
Yuri Mazursky <colomolo@users.noreply.github.com>
2025-01-30 11:55:20 +0000

    chore: add v2 wait method

--- lib/methods/bulk-action.ts
+++ lib/methods/bulk-action.ts
@@ -1,4 +1,4 @@
-import type { BulkActionPayload, BulkActionProps } from '../entities/bulk-action'
+import type { BulkActionPayload, BulkActionProps, BulkActionV2Payload } from '../entities/bulk-action'
 import type { PlainClientAPI } from '../plain/common-types'
 import type { AsyncActionProcessingOptions } from './action'
 import { pollAsyncActionStatus } from './action'
@@ -20,6 +20,23 @@ export async function waitForBulkActionProcessing<T extends BulkActionPayload = 
   return pollAsyncActionStatus<BulkActionProps>(
     async () =>
       plainClient.bulkAction.get<T>({
+        bulkActionId,
+        spaceId,
+        environmentId,
+      }),
+    options
+  )
+}
+
+/** Waits for a BulkAction V2 status to be either succeeded or failed.
+ * Used by the Plain client */
+export async function waitForBulkActionV2Processing<T extends BulkActionV2Payload = any>(
+  { plainClient, spaceId, environmentId, bulkActionId }: PlainOptions,
+  options?: AsyncActionProcessingOptions
+): Promise<BulkActionProps<T>> {
+  return pollAsyncActionStatus<BulkActionProps>(
+    async () =>
+      plainClient.bulkAction.getV2({
         bulkActionId,
         spaceId,
         environmentId,
--- test/integration/bulk-action-integration.test.ts
+++ test/integration/bulk-action-integration.test.ts
@@ -7,10 +7,11 @@ import type {
   BulkActionValidatePayload,
 } from '../../lib/contentful-management'
 import type { Environment, Space } from '../../lib/contentful-management'
-import { waitForBulkActionProcessing } from '../../lib/methods/bulk-action'
+import { waitForBulkActionProcessing, waitForBulkActionV2Processing } from '../../lib/methods/bulk-action'
 import { TestDefaults } from '../defaults'
 import { getDefaultSpace, initPlainClient, timeoutToCalmRateLimiting } from '../helpers'
 import { makeLink, makeVersionedLink } from '../utils'
+import { PublishBulkActionV2Payload, ValidateBulkActionV2Payload } from '../../lib/entities/bulk-action'
 
 describe('BulkActions Api v1', () => {
   let testSpace: Space
@@ -280,7 +281,7 @@ describe('BulkActions Api v2 (Plain Client only)', () => {
       ],
     })
 
-    const bulkActionCompleted = await waitForBulkActionProcessing<BulkActionPublishPayload>({
+    const bulkActionCompleted = await waitForBulkActionV2Processing<PublishBulkActionV2Payload<"add">>({
       ...defaultParams,
       plainClient,
       bulkActionId: bulkActionInProgress.sys.id,
@@ -303,7 +304,7 @@ describe('BulkActions Api v2 (Plain Client only)', () => {
       ],
     })
 
-    const bulkActionCompleted = await waitForBulkActionProcessing<BulkActionUnpublishPayload>({
+    const bulkActionCompleted = await waitForBulkActionV2Processing<PublishBulkActionV2Payload<'remove'>>({
       ...defaultParams,
       plainClient,
       bulkActionId: bulkActionInProgress.sys.id,
@@ -326,7 +327,7 @@ describe('BulkActions Api v2 (Plain Client only)', () => {
       ],
     })
 
-    const bulkActionCompleted = await waitForBulkActionProcessing<BulkActionValidatePayload>({
+    const bulkActionCompleted = await waitForBulkActionV2Processing<ValidateBulkActionV2Payload>({
       ...defaultParams,
       plainClient,
       bulkActionId: bulkActionInProgress.sys.id,
