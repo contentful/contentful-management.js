dcd05a5d1b83f51f4ed603f57442664dab57a174
Benedikt Rötsch <benedikt@hashbite.net>
2025-02-13 17:13:34 +0100

    test: quick node and browser e2e test after build

---
+++ test/output-integration/browser/vitest.setup.ts
@@ -0,0 +1,30 @@
+import { beforeAll, beforeEach, afterAll } from 'vitest'
+
+import puppeteer, { Browser, Page } from 'puppeteer'
+import path from 'path'
+
+let browser: Browser
+let page: Page
+
+beforeAll(async () => {
+  browser = await puppeteer.launch({
+    headless: true,
+  })
+})
+
+beforeEach(async () => {
+  page = await browser.newPage()
+
+  page.on('console', (msg) => console.log('CONSOLE LOG:', msg.text))
+  page.on('error', (err) => console.log('CONSOLE ERROR:', err, err.message))
+
+  await page.goto(`file:${path.join(__dirname, 'public/index.html')}`)
+
+  await page.waitForSelector('#contentful-management-loaded', { timeout: 5_000 })
+})
+
+afterAll(async () => {
+  await browser.close()
+})
+
+export { page }
