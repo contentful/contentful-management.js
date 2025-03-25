2a9faa31d815ddc594ded366e1995ccd8db32669
Benedikt Rötsch <benedikt@hashbite.net>
2025-03-25 12:52:43 +0100

    fix: remove confusing outdated ClientParams type and finally export the type for the client creation options

--- lib/index.ts
+++ lib/index.ts
@@ -44,11 +44,6 @@ interface UserAgentParams {
   feature?: string
 }
 
-/**
- * @deprecated
- */
-export type ClientParams = RestAdapterParams & UserAgentParams
-
 export type ClientOptions = UserAgentParams & XOR<RestAdapterParams, AdapterParams>
 
 /**
