708d5646ad005e867a964e5774733462e098c31c
Andi Pätzold <andreas.paetzold@contentful.com>
2025-03-25 14:38:15 +0100

    fix: add `version` param to patch method of plain client (#2547)
    * fix: add `version` param to patch method of plain client
    
    * chore: run prettier

--- lib/plain/common-types.ts
+++ lib/plain/common-types.ts
@@ -283,7 +283,7 @@ export type PlainClientAPI = {
       headers?: RawAxiosRequestHeaders
     ): Promise<EntryProps<T>>
     patch<T extends KeyValueMap = KeyValueMap>(
-      params: OptionalDefaults<GetSpaceEnvironmentParams & { entryId: string }>,
+      params: OptionalDefaults<GetSpaceEnvironmentParams & { entryId: string; version: number }>,
       rawData: OpPatch[],
       headers?: RawAxiosRequestHeaders
     ): Promise<EntryProps<T>>
