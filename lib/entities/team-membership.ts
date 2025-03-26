eeb2e7b3778ff0a68ae37708dd0832862cb6eb11
Andi Pätzold <andreas.paetzold@contentful.com>
2025-03-26 15:04:03 +0100

    chore: prettier

--- lib/entities/team-membership.ts
+++ lib/entities/team-membership.ts
@@ -2,12 +2,7 @@ import copy from 'fast-copy'
 import { freezeSys, toPlainObject } from 'contentful-sdk-core'
 import enhanceWithMethods from '../enhance-with-methods'
 import { wrapCollection } from '../common-utils'
-import type {
-  DefaultElements,
-  MetaSysProps,
-  MakeRequest,
-  Link,
-} from '../common-types'
+import type { DefaultElements, MetaSysProps, MakeRequest, Link } from '../common-types'
 
 export type TeamMembershipProps = {
   /**
