91eef042f5e92607b18e8343a75a4ea6ba564c84
Laurence Bortfeld <472753+mrlaessig@users.noreply.github.com>
2025-10-31 10:42:36 +0100

    feat: add cursor based pagination

--- test/integration/entry-integration.test.ts
+++ test/integration/entry-integration.test.ts
@@ -26,6 +26,7 @@ import {
   updateReleaseWithEntries,
   updateReleaseEntryTitle,
 } from './utils/release-entry.utils'
+import { get } from 'lodash'
 
 describe('Entry Api', () => {
   afterAll(async () => await timeoutToCalmRateLimiting())
@@ -76,6 +77,25 @@ describe('Entry Api', () => {
           expect(response.items, 'items').ok
           expect(response.items).lengthOf(2)
         })
+    })
+
+    test('Gets entries with a cursor parameter', async () => {
+      return environment.getEntries({ cursor: true, limit: 1 }).then(async (response) => {
+        expect(response.items, 'items').ok
+        expect(response.items).lengthOf(1)
+        expect(response.items).lengthOf(1)
+        expect(response.pages?.next).to.be.string
+      })
+    })
+
+
+    test('Gets published entries with a cursor parameter', async () => {
+      return environment.getPublishedEntries({ cursor: true, limit: 1 }).then(async (response) => {
+        expect(response.items, 'items').ok
+        expect(response.items).lengthOf(1)
+        expect(response.items).lengthOf(1)
+        expect(response.pages?.next).to.be.string
+      })
     })
 
     test('Gets entries with a skip parameter', async () => {
@@ -714,11 +734,32 @@ describe('Entry Api', () => {
     beforeAll(async () => {
       plainClient = initPlainClient({ spaceId: TestDefaults.spaceId })
     })
+
     test('getPublished', async () => {
       const response = await plainClient.entry.getPublished({ environmentId: 'master' })
       expect(response.items[0].sys.firstPublishedAt).to.not.be.undefined
       expect(response.items[0].sys.publishedVersion).to.not.be.undefined
       expect(response.items[0].sys.publishedAt).to.not.be.undefined
+    })
+
+    test('getMany cursor', async () => {
+      const response = await plainClient.entry.getMany({
+        environmentId: 'master',
+        query: { limit: 1, cursor: true },
+      })
+
+      expect(response.items).toHaveLength(1)
+      expect(response.pages?.next).to.be.string
+    })
+
+    test('getPublished cursor', async () => {
+      const response = await plainClient.entry.getPublished({
+        environmentId: 'master',
+        query: { limit: 1, cursor: true },
+      })
+
+      expect(response.items).toHaveLength(1)
+      expect(response.pages?.next).to.be.string
     })
   })
 
