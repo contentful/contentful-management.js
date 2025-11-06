5bc5cf9744e2065617f66f783190222c79a3b9fd
Yuri Mazursky <colomolo@users.noreply.github.com>
2025-11-06 17:57:17 +0000

    feat: [TOL-2513][SPA-2443] Bulk actions v2 support (#2528)
    * feat: add bulk actions v2 support
    
    * chore: update payload types
    
    * fix: add separate methods for v2 bulk actions
    
    * fix: linting issues
    
    * test: add new methods tests
    
    * test: adjust tests
    
    * chore: add v2 wait method
    
    * chore: fix lint errors
    
    * chore: export types
    
    * chore: fix linter errors
    
    * test: tweak unpublish test
    
    * chore: fix prettier error
    
    * fix: linting
    
    * refactor: move test helpers to corresponding file
    
    * chore: fix lint
    
    * chore: linting fix
    
    * chore: another lint fix attempt
    
    ---------
    
    Co-authored-by: Sofia Margariti <sofia.margariti@contentful.com>

--- lib/export-types.ts
+++ lib/export-types.ts
@@ -85,6 +85,10 @@ export type {
   BulkActionType,
   BulkActionUnpublishPayload,
   BulkActionValidatePayload,
+  BulkActionV2Payload,
+  PublishBulkActionV2Payload,
+  UnpublishBulkActionV2Payload,
+  ValidateBulkActionV2Payload,
 } from './entities/bulk-action'
 export type {
   Comment,
