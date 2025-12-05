3ed28b49c795b642a17b8fa246d6fd46bce25227
Joshua Berry <joshua.berry@contentful.com>
2025-12-05 15:44:57 +0000

    feat: add referencingEntryId query parameter to Resource API [ARC-727]

--- test/unit/plain/resource.test.ts
+++ test/unit/plain/resource.test.ts
@@ -30,4 +30,83 @@ describe('Resource', () => {
       `/spaces/spaceId/environments/envId/resource_types/${resourceTypeId}/resources`,
     )
   })
+
+  test('getMany with referencingEntryId parameter', async () => {
+    const { httpMock, adapterMock } = setupRestAdapter(
+      Promise.resolve({ data: { items: [resourceMock] } }),
+    )
+    const plainClient = createClient({ apiAdapter: adapterMock }, { type: 'plain' })
+    const response = await plainClient.resource.getMany({
+      spaceId,
+      environmentId,
+      resourceTypeId,
+      query: {
+        query: 'test',
+        referencingEntryId: 'entry-123',
+      },
+    })
+
+    expect(response).toBeInstanceOf(Object)
+    expect(httpMock.get.mock.calls[0][0]).toBe(
+      `/spaces/spaceId/environments/envId/resource_types/${resourceTypeId}/resources`,
+    )
+    expect(httpMock.get.mock.calls[0][1]).toMatchObject({
+      params: {
+        query: 'test',
+        referencingEntryId: 'entry-123',
+      },
+    })
+  })
+
+  test('getMany with locale parameter', async () => {
+    const { httpMock, adapterMock } = setupRestAdapter(
+      Promise.resolve({ data: { items: [resourceMock] } }),
+    )
+    const plainClient = createClient({ apiAdapter: adapterMock }, { type: 'plain' })
+    const response = await plainClient.resource.getMany({
+      spaceId,
+      environmentId,
+      resourceTypeId,
+      query: {
+        'sys.urn[in]': '123,456',
+        locale: 'en-US',
+      },
+    })
+
+    expect(response).toBeInstanceOf(Object)
+    expect(httpMock.get.mock.calls[0][1]).toMatchObject({
+      params: {
+        'sys.urn[in]': '123,456',
+        locale: 'en-US',
+      },
+    })
+  })
+
+  test('getMany with multiple optional parameters', async () => {
+    const { httpMock, adapterMock } = setupRestAdapter(
+      Promise.resolve({ data: { items: [resourceMock] } }),
+    )
+    const plainClient = createClient({ apiAdapter: adapterMock }, { type: 'plain' })
+    const response = await plainClient.resource.getMany({
+      spaceId,
+      environmentId,
+      resourceTypeId,
+      query: {
+        query: 'search term',
+        locale: 'de-DE',
+        referencingEntryId: 'entry-456',
+        limit: 10,
+      },
+    })
+
+    expect(response).toBeInstanceOf(Object)
+    expect(httpMock.get.mock.calls[0][1]).toMatchObject({
+      params: {
+        query: 'search term',
+        locale: 'de-DE',
+        referencingEntryId: 'entry-456',
+        limit: 10,
+      },
+    })
+  })
 })
