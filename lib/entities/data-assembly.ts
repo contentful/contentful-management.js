cc2994503e48e2373e3cb1a21469020c0dc1ddd4
Michael Pham <michael.pham@contentful.com>
2026-03-12 14:47:09 -0500

    feat: add DataAssembly types and interfaces (#2918)

---
+++ lib/entities/data-assembly.ts
@@ -0,0 +1,100 @@
+import type { Link, MetadataProps } from '../common-types'
+
+export type DataAssemblyDataTypeField = {
+  id: string
+  name: string
+  type: string
+  required: boolean
+  source?: string
+}
+
+export type DataAssemblyLinkParameter = {
+  type: 'Link'
+  linkType: string
+  allowedContentTypes: string[]
+}
+
+export type DataAssemblyParameterConfig = Record<string, DataAssemblyLinkParameter>
+
+export type DataAssemblyGraphQLResolver = {
+  source: 'Contentful:GraphQL'
+  query: string
+  parameters?: Record<string, unknown>
+}
+
+export type DataAssemblyNestedResolver = {
+  source: `Contentful:DataAssembly:${string}`
+  parameters?: Record<string, unknown>
+}
+
+export type DataAssemblyResolverDefinition =
+  | DataAssemblyGraphQLResolver
+  | DataAssemblyNestedResolver
+
+export type DataAssemblyResolverConfig = Record<string, DataAssemblyResolverDefinition>
+
+export type DataAssemblyReturnMappingValue =
+  | string
+  | { [key: string]: DataAssemblyReturnMappingValue }
+
+export type DataAssemblyReturnMappingConfig = Record<string, DataAssemblyReturnMappingValue>
+
+export type DataAssemblySys = {
+  id: string
+  type: 'DataAssembly'
+  dataType: DataAssemblyDataTypeField[]
+  version: number
+  space: Link<string>
+  environment: Link<string>
+  createdBy: Link<string>
+  createdAt: string | Date
+  updatedAt: string | Date
+  updatedBy?: Link<string>
+  publishedAt?: string | Date
+  publishedVersion?: number
+  publishedCounter?: number
+  firstPublishedAt?: string | Date
+  publishedBy?: Link<string>
+  variant?: string
+}
+
+type DataAssemblyCommonProps = {
+  metadata: Pick<MetadataProps, 'tags'>
+  name: string
+  description: string
+  parameters: DataAssemblyParameterConfig
+  resolvers: DataAssemblyResolverConfig
+  return: DataAssemblyReturnMappingConfig
+}
+
+export type DataAssemblyProps = DataAssemblyCommonProps & {
+  sys: DataAssemblySys
+}
+
+// Create payload
+export type CreateDataAssemblyProps = DataAssemblyCommonProps & {
+  sys: {
+    type: 'DataAssembly'
+    variant?: string
+    dataType: DataAssemblyDataTypeField[]
+  }
+}
+
+// Update payload
+export type UpdateDataAssemblyProps = DataAssemblyCommonProps & {
+  sys: {
+    id: string
+    type: 'DataAssembly'
+    variant?: string
+    version: number
+    dataType: DataAssemblyDataTypeField[]
+  }
+}
+
+// Query options for getMany
+export type DataAssemblyQueryOptions = {
+  pageNext?: string
+  pagePrev?: string
+  limit?: number
+  'sys.id[in]'?: string
+}
